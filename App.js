76643afa92bc3a5816acd86c0c5ff6489d382e81
mikasaloli <februarth@Xokthilats-MacBook-Pro.local>
2019-12-06 12:47:23 +0700

    test

--- App.js
+++ App.js
@@ -67,7 +67,7 @@ export default class App extends Component {
         <View style={styles.btnStart}>
           <Button style={styles.transcript}
           onPress={this._startRecognition.bind(this)}
-          title="Start"></Button>
+          title="Startt"></Button>
         </View>
       </View>
     )
